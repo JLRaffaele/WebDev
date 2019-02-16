e7b6c4f988fcc0f1eb60444d308786c134493b65
JLRaffaele <battery.dc2@hotmail.com>
2019-02-16 00:54:05 -0800

    added week4 content

---
+++ Week4/webapp/src/components/Persons.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <h1 class="Table_Header">People Table</h1>
+    <table>
+
+        <tr>
+            <th>Firstname</th>
+            <th>Middle Initial</th> 
+            <th>Lastname</th>
+        </tr>
+
+        <tr>
+            <td>Jill</td>
+            <td>A</td> 
+            <td>Smith</td>
+        </tr>
+
+        <tr>
+            <td>Eve</td>
+            <td>F</td> 
+            <td>Jackson</td>
+        </tr>
+
+        <tr>
+            <td>Beth</td>
+            <td>S</td> 
+            <td>Wallace</td>
+        </tr>
+
+            <td>John</td>
+            <td>L</td> 
+            <td>Sullivan</td>
+        </tr>
+
+        </tr>
+            <td>Frank</td>
+            <td>T</td> 
+            <td>Max</td>
+        </tr>
+
+
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Students',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+
+<style>
+.Table_Header {
+    color: red;
+}
+
+#Main_Header {
+    background-color: blue;
+}
+
+h2 {
+    text-shadow: 2px 2px #000000;
+}
+</style>
---
+++ Week4/webapp/src/components/Students.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h1 class="Table_Header">Student Table</h1>
+            <table>
+                
+                <thead>
+                    <tr>
+                        <th>StudentID</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="student in students">
+                        <td>{{ student.studentID }}</td>
+                        <td>{{ student.email }}</td>
+                    </tr>
+                </tbody>
+            </table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Students',
+  mounted() {
+      this.students = getStudents();
+  },
+  data () {
+    return {
+        students: []
+    }
+  }
+}
+
+function initializeStudents() {
+    var data = getStudents();
+    displayStudents(data.students);
+}
+
+function getStudents() {
+    return JSON.parse(testStudents).students;
+}
+
+var testStudents = '{"students": [{"studentID": "1","email": "ASmith@email.com"},{"studentID": "2","email": "AnEmail@mail.com"}]}'
+</script>
+
+
+<style>
+
+</style>
